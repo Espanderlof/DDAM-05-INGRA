18bdc8ad6e3fa7de8a59f7321172e0d4f0143e6f
espanderlof <jaime717@hotmail.es>
2023-03-14 19:39:58 -0300

    pre-login
    previo a navigation a login.

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import { LoginView } from "./src/views/LoginView";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <PaperProvider>
+            <SafeAreaView style={{ flex: 1 }}>
+                <LoginView />
+            </SafeAreaView>
+        </PaperProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/views/LoginView.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+
+
+export const LoginView = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        console.log('btn login');
+    };
+
+    const handleSignUp = () => {
+        console.log('btn registra');
+    };
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                label="Correo electrónico"
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCompleteType="email"
+                textContentType="emailAddress"
+                style={styles.input}
+            />
+            <TextInput
+                label="Contraseña"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+                autoCompleteType="password"
+                textContentType="password"
+                style={styles.input}
+            />
+            <Button mode="contained" onPress={handleLogin} style={styles.button}>
+                Iniciar sesión
+            </Button>
+            <Button mode="outlined" onPress={handleSignUp} style={styles.button}>
+                Registrarme
+            </Button>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 16,
+        backgroundColor: '#DAD8D8',
+    },
+    input: {
+        marginBottom: 16,
+    },
+    button: {
+        marginBottom: 16,
+    },
+});
